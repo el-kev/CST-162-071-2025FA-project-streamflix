6201ee3426045252faf7e8ad58306d46c015019c
el-kev <kevin.castro@owl.ucc.edu>
2025-12-15 21:03:17 -0500

    Create Series class extending Media

---
+++ src/Series.java
@@ -0,0 +1,17 @@
+public class Series exteds Media {
+  private int seasons;
+
+  public Series(String title, int year, String genre, int seasons) {
+    super(title, year, genre);
+    this.seasons = seasons;
+  }
+
+  public int getSeasons() {
+    return seasons;
+  }
+
+  @Override
+  public String getType() {
+    return "Series";
+  }
+}
