89252a18c97181da54a6742d975721f63c473eea
el-kev <kevin.castro@owl.ucc.edu>
2025-12-15 20:59:00 -0500

    Added abstract Media Class

---
+++ src/Media.java
@@ -0,0 +1,30 @@
+public abstract class Media {
+  private String title; 
+  private int year;
+  private String genre
+
+  public Media(String title, int year, String genre) {
+    this.title = title;
+    this.year = year;
+    this.genre = genre
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public int getYear() {
+  return year;
+  }
+
+  public String getGenre() {
+    return genre;
+  }
+
+  public abstract String getType();
+
+  @Override
+  public String toString() {
+    return getType() + ": " + title + " (" + year + ") - " + genre;
+  }
+}
