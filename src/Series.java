0d161185c770e0dd7fbb7575ab30f1ad0c525714
el-kev <kevin.castro@owl.ucc.edu>
2025-12-17 14:31:56 -0500

    Add Playable interface to Series class
    Implemented Playable interface and added play method.

--- src/Series.java
+++ src/Series.java
@@ -1,5 +1,4 @@
-public class Series extends Media {
-
+public class Series extends Media implements Playable {
     private int seasons;
 
     public Series(String title, int year, String genre, int seasons) {
@@ -14,5 +13,22 @@ public class Series extends Media {
     @Override
     public String getType() {
         return "Series";
+    }
+
+    @Override
+    public void play(Profile profile) {
+        int current = profile.getProgress(this);
+        int watch = 20;
+        int updated = current + watch
+
+        profile.setProgress(this, updated);
+
+        System.out.println("Now playing: " + getTitle());
+        System.out.println("Watched " + watch + " minutes (total : " + updated + ")");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | " + seasons + " seasons";
     }
 }
