553ceda6cead903feed6b995191cd8be8b620552
el-kev <kevin.castro@owl.ucc.edu>
2025-12-17 14:27:51 -0500

    Add play method to Movie class
    Implement play method to update progress and display status.

--- src/Movie.java
+++ src/Movie.java
@@ -14,4 +14,24 @@ public class Movie extends Media {
   public String getType() {
     return "Movie";
   }
+
+  @Override
+  public void play(Profile profile) {
+    int current = profile.getProgress(this);
+    int watch = 25;
+    int updated = Math.min(current + watch, durationMinutes);
+
+    profile.setProgress(this, updated);
+
+    System.out.println("Now playing: " + getTitle());
+    System.out.println("Progress: " + updated + "/" + durationMinutes + " minutes");
+    if (updated >= durationMinutes) {
+      System.out.println("Finished!");
+    } 
+  }
+
+  @Override
+  public String toString() {
+    return super.toString() + " | " + durationMinutes + " min";
+  }
 }
