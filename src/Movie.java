4c78cac8b7843868a04d940b28107800c7d07963
el-kev <kevin.castro@owl.ucc.edu>
2025-12-18 19:20:00 -0500

    Implement Playable interface in Movie class

--- src/Movie.java
+++ src/Movie.java
@@ -1,37 +1,37 @@
-public class Movie extends Media {
-  private int durationMinutes;
+public class Movie extends Media implements Playable {
+    private int durationMinutes;
 
-  public Movie(String title, int year, String genre, int durationMinutes) {
-    super(title, year, genre);
-    this.durationMinutes = durationMinutes;
-  }
-
-  public int getDurationMinutes() {
-    return durationMinutes;
-  }
-
-  @Override
-  public String getType() {
-    return "Movie";
-  }
-
-  @Override
-public void play(Profile profile) {
-    int current = profile.getProgress(this);
-    int watch = 25;
-    int updated = Math.min(current + watch, durationMinutes);
-
-    profile.setProgress(this, updated);
-
-    System.out.println("Now playing: " + getTitle());
-    System.out.println("Progress: " + updated + "/" + durationMinutes + " minutes");
-    if (updated >= durationMinutes) {
-        System.out.println("Finished!");
+    public Movie(String title, int year, String genre, int durationMinutes) {
+        super(title, year, genre);
+        this.durationMinutes = durationMinutes;
     }
-}
 
-  @Override
-  public String toString() {
-    return super.toString() + " | " + durationMinutes + " min";
-  }
+    public int getDurationMinutes() {
+        return durationMinutes;
+    }
+
+    @Override
+    public String getType() {
+        return "Movie";
+    }
+
+    @Override
+    public void play(Profile profile) {
+        int current = profile.getProgress(this);
+        int watch = 25;
+        int updated = Math.min(current + watch, durationMinutes);
+
+        profile.setProgress(this, updated);
+
+        System.out.println("Now playing: " + getTitle());
+        System.out.println("Progress: " + updated + "/" + durationMinutes + " minutes");
+        if (updated >= durationMinutes) {
+            System.out.println("Finished!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | " + durationMinutes + " min";
+    }
 }
